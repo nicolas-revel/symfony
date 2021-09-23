bfcc5bd1bc9d3119429f5639c63e03a6b3edab59
Nicolas Revel <nicolas.revel@laplateforme.io>
2021-09-23 16:08:56 +0200

    retrieving all articles from db and creating pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Entity\User;
+use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ *
+ */
 class HomeController extends AbstractController
 {
     /**
+     * @var PostRepository
+     */
+    private PostRepository $postRepository;
+
+    /**
+     * @var PaginatorInterface
+     */
+    private PaginatorInterface $paginator;
+
+    /**
+     * @param PostRepository $postRepository
+     * @param PaginatorInterface $paginator
+     */
+    public function __construct(PostRepository $postRepository, PaginatorInterface $paginator)
+    {
+        $this->postRepository = $postRepository;
+        $this->paginator = $paginator;
+    }
+
+    /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $unpaginatedPosts = $this->postRepository->getPaginatedPosts();
+        $posts = $this->paginator->paginate($unpaginatedPosts, $request->query->getInt('page', 1), 15);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'posts' => $posts,
         ]);
     }
 }
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -19,32 +19,9 @@ class PostRepository extends ServiceEntityRepository
         parent::__construct($registry, Post::class);
     }
 
-    // /**
-    //  * @return Post[] Returns an array of Post objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function getPaginatedPosts()
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+            ->getQuery();
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Post
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
